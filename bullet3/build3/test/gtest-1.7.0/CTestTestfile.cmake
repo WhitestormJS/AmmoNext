eb7dd018debbb21e4429956fd1cb814e9a995774
sasha240100 <alexbuzin88@gmail.com>
2017-01-05 16:37:35 +0200

    WIP

--- bullet3/build3/test/gtest-1.7.0/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Alex_2/Documents/GitHub/ammo.js/bullet3/test/gtest-1.7.0
-# Build directory: C:/Users/Alex_2/Documents/GitHub/ammo.js/bullet3/build/test/gtest-1.7.0
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
